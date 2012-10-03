457a1bb720f92095ef7f3bcca5c784f1394433e2
Gleb Peregud <gleber.p@gmail.com>
2012-10-03 10:53:13 +0200

    add expm package

---
+++ package.exs
@@ -0,0 +1,6 @@
+Expm.Package.new(name: "erlgit", description: "Convenience wrapper around git executable",
+                 version: "0.7.5", keywords: ["git", "wrapper", "vcs"], 
+                 maintainers: [[name: "Gleb Peregud", 
+                                email: "gleber.p@gmail.com"]],
+                 repositories: [[github: "gleber/erlgit"]])
+    
